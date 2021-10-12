f4528aa6cbc739962f7a68eb628935b571224e2b
stellaKK <35387152+stellaKK@users.noreply.github.com>
2021-10-12 18:14:10 -0400

    Updates

---
+++ precache-manifest.4969cb3d115350e128e4d0f619da4806.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c297124f28a058368bbf",
+    "url": "/HorseWatch/static/css/main.cb36d195.chunk.css"
+  },
+  {
+    "revision": "c297124f28a058368bbf",
+    "url": "/HorseWatch/static/js/main.c297124f.chunk.js"
+  },
+  {
+    "revision": "7c3aec79f3ae62290828",
+    "url": "/HorseWatch/static/js/1.7c3aec79.chunk.js"
+  },
+  {
+    "revision": "b13c81c712d7102492f7",
+    "url": "/HorseWatch/static/js/runtime~main.b13c81c7.js"
+  },
+  {
+    "revision": "c4205ef624a19b4109ad1afd75e7649a",
+    "url": "/HorseWatch/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5ce139797344049079094a882b92674f.js"
+  "/HorseWatch/precache-manifest.4969cb3d115350e128e4d0f619da4806.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/HorseWatch/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });

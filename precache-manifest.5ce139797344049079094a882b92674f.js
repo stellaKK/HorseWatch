0a5193b664a29139087dbc04fa11f476e8c1a5d7
stellaKK <35387152+stellaKK@users.noreply.github.com>
2021-10-12 17:07:38 -0400

    Updates

---
+++ precache-manifest.5ce139797344049079094a882b92674f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f1ebd1feb92c87f76a3",
+    "url": "/static/css/main.35787f2d.chunk.css"
+  },
+  {
+    "revision": "5f1ebd1feb92c87f76a3",
+    "url": "/static/js/main.5f1ebd1f.chunk.js"
+  },
+  {
+    "revision": "7c3aec79f3ae62290828",
+    "url": "/static/js/1.7c3aec79.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d142c944cb3c9d70dd2e8996728199cc",
+    "url": "/index.html"
+  }
+];
